701ddb3168f92559ce04a9fa6d7e115253542121
melbin thomas <melbin thomas>
2016-05-19 06:34:17 +0530

    Responsive images grunt plugin used

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,21 +3,57 @@ module.exports=function(grunt){
 
 	grunt.initConfig({
 		browserSync: {
-    			bsFiles: {
+    			files: {
         			src : ['*.html',
-        			        'css/*.css'
+        			        'css/*.css',
+                            'images/img/*.{png,gif,jpg}'
         			]
     			},
     			options: {
         			server: {
             			baseDir: "./"
-  	     		 }
+  	     		}
 			 }  		 
-		}
+		},
+        responsive_images:{
+            dev :{
+                    
+                    options:{
+                        engine : "im",
+                        newFilesOnly :false,
+                        sizes :[{
+                            name: "small",
+                            width: 150,
+                            height: 100,
+                            aspectRatio:false
+                        },{
+                            name: "medium",
+                            width: 350,
+                            height: 350,
+                            upscale: true,
+                            aspectRatio: false
+
+                        },{
+                            name :"large",
+                            upscale: true,
+                            width: 1000,
+                            height: 600,
+                            aspectRatio:false
+
+                        }]
+                    },
+                    files :[{
+                        expand: true,
+                        src : ['img/**/*.{gif,jpg,png}'],
+                        cwd : './',
+                        dest: 'images/'
+                    }]
+            }
+        }
 
 	});
-	
 	grunt.registerTask('default',[
-           'browserSync'
+           'browserSync',
+           'responsive_images'
 		]);
 }
